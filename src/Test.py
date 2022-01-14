d55bdd0cebfa06fd0792af359c46d97839226c4f
kaedenbrinkman <49798040+kaedenbrinkman@users.noreply.github.com>
2022-01-14 15:38:06 -0800

    Update Test.py

--- src/Test.py
+++ src/Test.py
@@ -1,7 +1,14 @@
 # This file is for testing the other ones
 
 from TeslaController import TeslaVehicle
-import VCSEC_pb2
-
 
 vehicle = TeslaVehicle("00:00:00:00:00:00", "SOME_NAME")
+
+msg = vehicle.whitelistOp()
+print(msg)
+
+msg = vehicle.vehiclePublicKeyMsg()
+print(msg)
+
+msg = vehicle.unlockMsg()
+print(msg)
