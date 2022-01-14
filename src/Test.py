92275df3592372932f6f25d0a0bf8993d1fae090
kaedenbrinkman <49798040+kaedenbrinkman@users.noreply.github.com>
2022-01-14 14:43:10 -0800

    Removed testing code, no longer needed

--- src/Test.py
+++ src/Test.py
@@ -5,19 +5,3 @@ import VCSEC_pb2
 
 
 vehicle = TeslaVehicle("00:00:00:00:00:00", "SOME_NAME")
-
-# private_key = vehicle.getPrivateKey()
-# public_key = vehicle.getPublicKey()
-# print("Private key: {}".format(private_key))
-# print("Public key: {}".format(public_key))
-
-# init_msg = vehicle.initMsg()
-# print("Init message: {}".format(init_msg))
-# test_msg = vehicle.encrypt_message(init_msg)
-# print("Test message: {}".format(test_msg))
-
-response = b'\x00\x04\x1a\x02\x18\x02'
-print(response)
-msg = VCSEC_pb2.FromVCSECMessage()
-msg.ParseFromString(bytes(response))
-print(msg)
